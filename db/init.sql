0ac60a7e3afc2bdc1d67cbb5a5cb746a8e34a8ed
Agustindt <agustindt95@gmail.com>
2022-04-28 13:43:04 -0300

    first commit

---
+++ db/init.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE drinks;
+use drinks;
+
+CREATE TABLE wines
+(
+    name       VARCHAR(20),
+    color  VARCHAR(20)
+);
+
+INSERT INTO wines
+    (name, color)
+VALUES ('Malbec', 'Tinto'),
+       ('Tempranillo', 'Tinto'),
+       ('Syrah', 'Tinto'),
+       ('Chenin', 'Blanco'),
+       ('Chardonnay', 'Blanco'),
+       ('DittoMasso','Tinto'),
+       ('Montiel','Berreta');
